4d83a2926731233fb89bb78529dd78412e967cb6
Syed Zain Badar <49241055+zain2498@users.noreply.github.com>
2023-09-26 02:29:22 +0500

    add responsiveness

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9e445ce6c9f85650a3e9b9411932748f",
-"index.html": "a1dccfea9e611896146be4aa31a6f453",
-"/": "a1dccfea9e611896146be4aa31a6f453",
-"main.dart.js": "9ef070285abca88ab732e0daaa1a1019",
+"index.html": "5eab73f3de7e9e53b17445a27ed303ca",
+"/": "5eab73f3de7e9e53b17445a27ed303ca",
+"main.dart.js": "44ecea737939fc6ee84b194abedf74b7",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
