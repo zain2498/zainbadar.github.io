f2ece50b161e43556d9372ac31041464f8ee8c1b
Syed Zain Badar <49241055+zain2498@users.noreply.github.com>
2023-09-26 12:51:15 +0500

    footer work start

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9e445ce6c9f85650a3e9b9411932748f",
-"index.html": "5eab73f3de7e9e53b17445a27ed303ca",
-"/": "5eab73f3de7e9e53b17445a27ed303ca",
-"main.dart.js": "44ecea737939fc6ee84b194abedf74b7",
+"index.html": "9c4238ca8f9481fd147facad172d9337",
+"/": "9c4238ca8f9481fd147facad172d9337",
+"main.dart.js": "0033be0b376c85f30701379bd974e493",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
